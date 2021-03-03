edcd2dc452ea58c2c88179f78e8aa8dd750946c0
kolachhapati <47025358+kolachhapati@users.noreply.github.com>
2021-03-03 14:26:29 +0545

    Add project files.

---
+++ Data/Hotel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApi_v5.Data
+{
+    public class Hotel
+    {
+
+    }
+}
