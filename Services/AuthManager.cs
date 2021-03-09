cd7fb9a992c81faf2ce5366affc10909eeb4c1c5
kolachhapati <47025358+kolachhapati@users.noreply.github.com>
2021-03-09 15:05:33 +0545

    Token Based authentication Added
    JWT auth added

--- Services/AuthManager.cs
+++ Services/AuthManager.cs
@@ -1,21 +1,82 @@
-﻿using NetCoreWebApi_v5.Models;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NetCoreWebApi_v5.Data;
+using NetCoreWebApi_v5.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NetCoreWebApi_v5.Services
 {
     public class AuthManager : IAuthManager
     {
-        public Task<string> CreateToken()
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private ApiUser _user;
+        public AuthManager(UserManager<ApiUser> userManager,
+                            IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
         }
 
-        public Task<bool> ValidateUser(UserDTO user)
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
         {
-            throw new NotImplementedException();
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var tokenExpiry = DateTime.Now.AddHours(4);
+            
+            var token = new JwtSecurityToken(
+                  issuer: jwtSettings.GetSection("Issuer").Value,
+                  claims: claims,
+                  expires: tokenExpiry,
+                  signingCredentials: signingCredentials
+                  );
+            return token;
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = "ab50d39c-07fb-4b75-b568-b729323abccc";
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        public async Task<bool> ValidateUser(UserDTO userDto)
+        {
+            _user = await _userManager.FindByNameAsync(userDto.EmailAddress);
+            return (_user != null && await _userManager.CheckPasswordAsync(_user, userDto.Password));
         }
     }
 }
